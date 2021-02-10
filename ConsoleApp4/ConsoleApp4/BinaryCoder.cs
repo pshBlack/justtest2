ac7ef7904dc2b11c0b66c80d7d5ca9be85f24645
Skei4ol <62964286+pshBlack@users.noreply.github.com>
2021-02-10 21:25:00 +0200

    Add files via upload
    This sh*t work

---
+++ ConsoleApp4/ConsoleApp4/BinaryCoder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BinaryCoder
+{
+    class BinaryCoder
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Write number to convert in binary(max number is 4294967295)");
+                uint code = Convert.ToUInt32(Console.ReadLine());
+                Console.WriteLine("Your binary number is: " + Convert.ToString(code, 2));
+            }
+            catch(OverflowException Exception)
+            {
+                Console.WriteLine("Error: Your number is too big, or it's less than 1.");
+            }
+        }
+    }
+}
