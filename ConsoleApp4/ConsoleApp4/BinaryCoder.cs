5d411be09f0179a2e977ad510fcba8c6f866a684
Skei4ol <62964286+pshBlack@users.noreply.github.com>
2021-02-10 21:17:10 +0200

    Add files via upload

---
+++ ConsoleApp4/ConsoleApp4/BinaryCoder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BinaryCoder
+{
+    class BinaryCoder
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Write number to convert in binary(max number is between –2147483648 and 2147483647)");
+                int code = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Your binary number is: " + Convert.ToString(code, 2));
+            }
+            catch(OverflowException)
+            {
+                Console.WriteLine("Error: Your number is too big.");
+            }
+        }
+    }
+}
